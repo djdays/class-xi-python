2f0a5641a8fbcffad6a0a169b85662623e28774b
DJ Days <sridhanajeyan@gmail.com>
2020-10-21 18:51:28 +0530

    Arthimetic Operation

---
+++ assignment_19-10-2020.py
@@ -0,0 +1,26 @@
+#  Get the value of five numbers from  the user
+
+num1 = int(input("Enter number1:   "))
+num2 = int(input("Enter number2:   "))
+num3 = int(input("Enter number3:   "))
+num4 = int(input("Enter number4:   "))
+num5 = int(input("Enter number5:   "))
+
+#   To find the addition of 5 Numbers
+addition = num1 + num2 + num3 + num4 + num5
+print("Addition = ", addition)
+
+
+# Subtraction: num1-num2
+subtraction = num1 - num2
+print("Subtraction = ", subtraction)
+
+
+# Multiplication: num1 * num2
+multiplication = num1 * num2
+print("Multiplication = ",multiplication)
+
+
+# Division: num1 / num2
+division = num1/num2
+print("Division = ",division)
