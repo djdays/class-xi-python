ac2cf2845d178bad70eb10b95e9ca09b1f6c4c3d
DJ Days <sridhanajeyan@gmail.com>
2021-03-09 02:27:26 +0000

    Fixed rebase issue

--- assignment_23-10-2020.py
+++ assignment_23-10-2020.py
@@ -1,8 +1,14 @@
 # To find simple interest 
 # with given Principle (p), No. of Years (n) and Rate of Interest (r)
+<<<<<<< HEAD
 p = float(input("Enter principle: "))
 n = float(input("Enter number of years: "))
 r = float(input("Enter the rate of interest: "))
+=======
+p = int(input("Enter principle: "))
+n = int(input("Enter number of years: "))
+r = int(input("Enter the rate of interest: "))
+>>>>>>> 813d6e2... Find Simple Interest with given Pnr
 
 interest = (p * n * r ) / 100
 
